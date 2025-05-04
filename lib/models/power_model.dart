e1c7d64fde6db7edaadcb1071490ba4dd7563c86
The777Bot <ubadahme@gmail.com>
2025-05-04 14:55:46 +0500

    Add PowerModel class for power selection functionality

---
+++ lib/models/power_model.dart
@@ -0,0 +1,15 @@
+class PowerModel {
+  final String name;
+  final String type;
+  final String trigger;
+  final String appearanceEffect;
+  final String weakness;
+
+  PowerModel({
+    required this.name,
+    required this.type,
+    required this.trigger,
+    required this.appearanceEffect,
+    required this.weakness,
+  });
+}
